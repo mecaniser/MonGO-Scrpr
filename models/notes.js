68c63cd486daf570b8218a7ced0ee9523833abc4
Sergio <sergioburca@gmail.com>
2018-04-26 04:38:08 -0400

    Schema Files, work as intended

---
+++ models/notes.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var NotesSchema = new Schema({
+  name: {
+    type: String
+  },
+  body: {
+    type: String,
+    required: true
+  }
+});
+
+var Notes = mongoose.model("Notes", NotesSchema);
+
+module.exports = Notes;
+
